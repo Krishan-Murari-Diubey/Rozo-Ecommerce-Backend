7c70851127924e78ed941a998defc16b42b687e6
krishna <dubeykrishanmurari6@gmail.com>
2024-04-07 23:17:58 +0530

    final commit

---
+++ Routes/Auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { createUser, loginUser, checkAuth, resetPasswordRequest, logout, resetPassword } = require('../Controller/Auth');
+const passport = require('passport');
+
+
+const router = express.Router();
+  
+router.post('/signup', createUser)
+.post('/login', passport.authenticate('local'), loginUser)
+.get('/check',passport.authenticate('jwt'), checkAuth)
+.get('/logout', logout)
+.post('/reset-password-request', resetPasswordRequest)
+.post('/reset-password', resetPassword)
+
+exports.router = router;
